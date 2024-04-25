76bbc2adf5b8e18e3c8a26a04fe97fb38ecf5bcb
commonwebsite <harendrabaghel532@gmail.com>
2024-04-25 07:52:29 +0000

    commit

---
+++ MemeGen/meme.js
@@ -0,0 +1,9 @@
+// CAN\NVAS.js plugin
+// ninivert, december 2016
+(function (window, document) {
+    /**
+    * CAN\VAS Plugin - Adding line breaks to canvas
+    * @arg {string} [str=Hello World] - text to be drawn
+    * @arg {number} [x=0]             - top left x coordinate of the text
+    * @arg {number} [y=textSize]      - top left y coordinate of the text
+   
