651376ca0b5f74da2c81c48c10dfc5bb7e0fabf6
dumbgitpage <harendrabaghel532@gmail.com>
2024-12-27 20:53:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// Example script to demonstrate adding interactivity if needed.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Baghels' Job Portal Loaded");
+});
